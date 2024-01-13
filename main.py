ac46761c7f7755a2ec2f0845400983da63c606c7
Qubiik <115170239+Qubiik@users.noreply.github.com>
2024-01-13 07:27:06 +0000

    Короче идея с ботом провалилась

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
-import telebot
+
 import g4f
 
-TOKEN = '6928486878:AAF4aslLmvrehZG6IoQBqimRZVOXrisLbNc'
+zapros = input("Введите запрос: ")
 
-bot = telebot.TeleBot(TOKEN)
-
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    bot.reply_to(message, "Привет! Напиши мне что-нибудь.")
-
-@bot.message_handler(func=lambda message: True)
-def gpt(message):
-  zapros = message.text
-  response = g4f.ChatCompletion.create(
-    model="gpt-4-0613",
-    provider=g4f.Provider.GeekGpt,
-    messages=[{"role": "user", "content": zapros}],
-  )
-  bot.reply_to(response)
-bot.polling()
+response = g4f.ChatCompletion.create(
+  model="gpt-4-0613",
+  provider=g4f.Provider.GeekGpt,
+  messages=[{"role": "user", "content": zapros}],
+)
+print(response)
